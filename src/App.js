66221b50dd9817f106fd7aef83e7ed4b7e975a21
Frokesy <akindeleayanfeoluwa@gmail.com>
2022-10-11 23:36:54 +0100

    project setup

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ function App() {
     <div className="App flex items-center bg-neutral-800 justify-center h-screen">
       <header className="App-header">
         <p className="text-[24px] text-white">
-          Hello world! This is a react with tailwind boilerplate setup by yours sincerely 
+          Hello world! This is finance app clone 
         </p>
       </header>
     </div>
